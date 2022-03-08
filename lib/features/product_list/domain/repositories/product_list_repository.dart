5b1f4a91c482e198aef2be62ce700fa5010f882f
alirezat66 <alirezataghizadeh66@gmail.com>
2022-03-08 19:20:51 +0300

    create productlistrepository

--- lib/features/product_list/domain/repositories/product_list_repository.dart
+++ lib/features/product_list/domain/repositories/product_list_repository.dart
@@ -1,1 +1,7 @@
-abstract class ProductListRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:shop_online_clean/core/error/failures.dart';
+import 'package:shop_online_clean/features/product_list/domain/entities/product.dart';
+
+abstract class ProductListRepository {
+  Future<Either<Failure, Product>>? fetchAllProducts(int page, int perPage);
+}
